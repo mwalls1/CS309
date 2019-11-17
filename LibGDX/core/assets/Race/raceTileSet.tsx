4510fb0296138633211100393fe0096ca7723a5c
Noah Thompson <noah17@iastate.edu>
2019-11-16 18:11:05 -0600

    Added track tileset and tmx. Fixed rendering of map

--- LibGDX/core/assets/Race/raceTileSet.tsx
+++ LibGDX/core/assets/Race/raceTileSet.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.5" name="raceTileSet" tilewidth="32" tileheight="32" tilecount="100" columns="10">
- <image source="raceTileSet.png" width="320" height="320"/>
+ <image source="raceTileSet.png" width="320" height="160"/>
 </tileset>
--- LibGDX/core/src/Race/RaceScreen.java
+++ LibGDX/core/src/Race/RaceScreen.java
@@ -5,10 +5,8 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.maps.MapLayers;
+import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -17,83 +15,80 @@ public class RaceScreen implements Screen{
 private TiledMap map;
 private Game game;
 private OrthogonalTiledMapRenderer renderer;
-private Stage stage;
 private OrthographicCamera camera;
-private SpriteBatch batch;
-private TiledMapTileLayer road;
-private TiledMapTileLayer wall;
-
+private Stage stage;
+private MapLayer road;
+private MapLayer wall;
 public RaceScreen(Game game)
 {
 	this.game = game;
 	create();
 }
-	@Override
-	public void show() {
-		// TODO Auto-generated method stub
-		
-	}
 
-	@Override
-	public void render(float delta) {
-		Gdx.gl.glClearColor((float)1/255, (float)1/255, (float)1/255, 0);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		stage.act();
-		stage.draw();
-		camera.update();
-		batch.setProjectionMatrix(camera.combined);
-		renderer.setView(camera);
-		renderer.render();
-	}
 
-	@Override
-	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
-		
-	}
 
-	@Override
-	public void pause() {
-		// TODO Auto-generated method stub
-		
-	}
 
-	@Override
-	public void resume() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void hide() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	public void create()
-	{
-		map = new TmxMapLoader().load("Race/testTrack.tmx");
-	
-		Gdx.graphics.setFullscreenMode(Gdx.graphics.getDisplayMode());
-		camera = new OrthographicCamera(Gdx.graphics.getWidth()*4, Gdx.graphics.getHeight()*4);
-		
-		batch = new SpriteBatch();
-		
-		MapLayers mapLayers = map.getLayers();
-		wall = (TiledMapTileLayer) mapLayers.get("Wall");
-		road = (TiledMapTileLayer) mapLayers.get("Road");
-		
-		renderer = new OrthogonalTiledMapRenderer(map);
-		
-		stage = new Stage();
-		
-        Gdx.input.setInputProcessor(stage);
-	}
-
+public void create()
+{
+	  float w = Gdx.graphics.getWidth();
+      float h = Gdx.graphics.getHeight();
+      stage = new Stage();
+      camera = new OrthographicCamera();
+      camera.setToOrtho(false,w,h);
+      camera.update();
+      map = new TmxMapLoader().load("Race/track1.tmx");
+      road = map.getLayers().get("Road");
+      wall = map.getLayers().get("wall");
+      renderer = new OrthogonalTiledMapRenderer(map);
+      Gdx.input.setInputProcessor(stage);
 }
+
+
+
+@Override
+public void show() {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void render(float delta) {
+	// TODO Auto-generated method stub
+	 Gdx.gl.glClearColor(1, 0, 0, 1);
+     Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+     camera.update();
+     renderer.setView(camera);
+     renderer.render();
+}
+
+@Override
+public void resize(int width, int height) {
+}
+
+@Override
+public void pause() {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void resume() {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void hide() {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void dispose() {
+	// TODO Auto-generated method stub
+	
+}
+}
+
+	
