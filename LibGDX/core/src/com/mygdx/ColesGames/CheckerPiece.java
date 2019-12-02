a0175a7428703e64c0a6efdb21dd2ff9b504cfed
Taylor <taylorweil98@gmail.com>
2019-12-02 14:16:42 -0600

    checker piece can now move and jump

--- LibGDX/core/src/com/mygdx/ColesGames/CheckerPiece.java
+++ LibGDX/core/src/com/mygdx/ColesGames/CheckerPiece.java
@@ -8,15 +8,15 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public class CheckerPiece {
 
-	private boolean color; //false for RED, true for YELLOW
+	private int value; //1 for RED, 2 for YELLOW, 3 for Possible Move
 	private int[] position; //position[COL][ROW]
 	private boolean king; //True if the piece is king'd
 	private Sprite checker;
 	ArrayList<int[]> possibleMoves;
 	ArrayList<Sprite> possibleMoveSprites;
 	
-	public CheckerPiece(boolean color, int[] position) {
-		this.color = color;
+	public CheckerPiece(int value, int[] position) {
+		this.value = value;
 		this.position = position;
 		this.king = false;
 		updateSkin();
@@ -30,22 +30,27 @@ public class CheckerPiece {
 	
 	public int getRow() {return position[1];}
 	
-	public boolean getColor() {return color;}
+	public int getValue() {return value;}
+	
+	public ArrayList<int[]> getPosibbleMoves(){ return possibleMoves;}
 	
 	public ArrayList<Sprite> getPossibleMoveSprites(){ return possibleMoveSprites;}
 	
 	public Sprite updateSkin() {
-		if(!color) {
+		if(value == 1) {
 			if(!king) {
 				checker = new Sprite(new Texture("redCircle.png"));
 			}
 			else checker = new Sprite(new Texture("redCircleKing.png"));
 		}
-		else {
+		else if (value == 2){
 			if(!king) {
 				checker = new Sprite(new Texture("yellowCircle.png"));
 			}
 			else checker = new Sprite(new Texture("yellowCircleKing.png"));
+		}
+		else {
+			checker = new Sprite(new Texture("download.png"));
 		}
 		checker.setPosition((float) ((position[0])*74.5+3+(new Random().nextInt(11)+3)), (float) (position[1]*74.0+(new Random().nextInt(11)+3))); //When random is 0 piece will touch left side, when 17 right side
 		return checker;
@@ -53,7 +58,7 @@ public class CheckerPiece {
 	
 	public Sprite updateLocaiton(int[] position) {
 		this.position = position;
-		checker.setPosition(position[0]*75, position[1]*75);
+		checker.setPosition((float) ((position[0])*74.5+3+(new Random().nextInt(11)+3)), (float) (position[1]*74.0+(new Random().nextInt(11)+3)));
 		return checker;
 	}
 	
@@ -61,10 +66,10 @@ public class CheckerPiece {
 		possibleMoves = arr;
 		possibleMoveSprites.clear();
 		for (int[] move : arr) {
-			Sprite newMove = new Sprite(new Texture("download.png"));
-			newMove.setSize(60, 60);
-			newMove.setPosition((float) ((move[1]*2+move[0]%2)*74.5+12), (float) (move[0]*74.0+9));
-			possibleMoveSprites.add(newMove);
+//			Sprite newMove = new Sprite(new Texture("download.png"));
+//			newMove.setSize(60, 60);
+//			newMove.setPosition((float) ((move[1]*2+move[0]%2)*74.5+12), (float) (move[0]*74.0+9));
+//			possibleMoveSprites.add(newMove);
 		}
 	}
 }
--- LibGDX/core/src/com/mygdx/ColesGames/Checkers.java
+++ LibGDX/core/src/com/mygdx/ColesGames/Checkers.java
@@ -38,10 +38,13 @@ public class Checkers extends Game implements Screen {
 	private int mousex;
 	private int mousey;
 	
-	private String[] colors = {"red","yellow"};
+	private String[] colors = {"red","yellow","posMove"};
 	private HashMap<String,ArrayList<CheckerPiece>> pieces = new HashMap<String,ArrayList<CheckerPiece>>();
 	private int[][] gameBoardArr;
 	ArrayList<Sprite> possibleMoves;
+	String[] currentPiece; 
+	
+	
 	public Checkers(Game game) {
 		this.game = game;
 		create();
@@ -57,9 +60,10 @@ public class Checkers extends Game implements Screen {
 		spriteCheckersBoard.setSize(600, 600);
 		gameBoardArr = new int[8][4];
 		//Initialize game
-		if (Constants.playerNumber == 0) isTurn = true;
+		if (Constants.playerNumber == 0) isTurn = true; Constants.playerNumber = 1;
 		isGameOver = false;
 		possibleMoves = new ArrayList<Sprite>();
+		currentPiece = null;
 		//Initialize Colors
 		for (String color : colors) pieces.put(color, new ArrayList<CheckerPiece>());
 		setBoard();
@@ -81,13 +85,15 @@ public class Checkers extends Game implements Screen {
 		for (String color : colors) {
 			for (CheckerPiece checker : pieces.get(color)) {
 				checker.getSkin().draw(batch);
-				if (checker.getColor()) gameBoardArr[checker.getRow()][checker.getCol()] = 2; //2 for yellow
-				else gameBoardArr[checker.getRow()][checker.getCol()] = 1; //1 for red
+				gameBoardArr[checker.getRow()][checker.getCol()] = checker.getValue(); //1 for red, 2 for yellow, 3 for possible move
 			}
 		}
-		for (CheckerPiece checker : pieces.get("red")) { //Finds all possible moves for each piece
-			checker.setPossibleMoves(getPossibleMoves(checker.getCol(), checker.getRow()));
+		for (String color : colors) {
+			for (CheckerPiece checker : pieces.get(color)) { //Finds all possible moves for each piece
+				checker.setPossibleMoves(getPossibleMoves(checker.getCol(), checker.getRow()));
+			}
 		}
+			
 		handleKeys();
 		
 		//Handle the players turn
@@ -95,14 +101,42 @@ public class Checkers extends Game implements Screen {
 			int col = (int) (((Gdx.input.getX()-3)/75)/2);
 			int row = (int) 7-(Gdx.input.getY()/75);
 			System.out.println(col + " " + row );
-			if(gameBoardArr[row][col] == 1) {
-				possibleMoves = getPieceByPosition(row, col).getPossibleMoveSprites();
+			//Check to see if piece is clicked on
+			if(gameBoardArr[row][col] == Constants.playerNumber || gameBoardArr[row][col] == 2) {
+				pieces.get("posMove").clear();
+				currentPiece = getPieceByPosition(row, col).split(" ");
+				CheckerPiece piece = pieces.get(currentPiece[0]).get(Integer.parseInt(currentPiece[1]));
+				possibleMoves = piece.getPossibleMoveSprites();
+				for (int[] arr : piece.getPosibbleMoves()) {
+					pieces.get("posMove").add(new CheckerPiece(3,new int[] {arr[1]*2+arr[0]%2,arr[0]}));
+				}
 				System.out.println(possibleMoves.toString());
+			}
+			//Check to see if possible move is clicked on and moves piece
+			else if(gameBoardArr[row][col] == 3) {
+				//Checks to see if move was a jump
+				CheckerPiece currentChecker = pieces.get(currentPiece[0]).get(Integer.parseInt(currentPiece[1]));
+				if(Math.abs(currentChecker.getRow() - row) == 2) {
+					boolean leftOrRight = ((col)-currentChecker.getCol()) == 1;
+					int jumpedCol = 0;// = upOrDown * Math.abs(currentChecker.getRow()%2-1)+currentChecker.getCol();
+					if (!leftOrRight && currentChecker.getRow()%2 == 0) jumpedCol = -1 + currentChecker.getCol();
+					else if (leftOrRight && currentChecker.getRow()%2 == 1) jumpedCol = 1 + currentChecker.getCol();
+					else jumpedCol = currentChecker.getCol();
+					int jumpedRow = (row+currentChecker.getRow())/2;
+					System.out.println("JumpedCol: "+jumpedCol+",JumpedRow: " + jumpedRow);
+					String[] jumpedPiece = getPieceByPosition(jumpedRow,jumpedCol).split(" ");
+					pieces.get(jumpedPiece[0]).remove(Integer.parseInt(jumpedPiece[1]));
+				}
+				currentChecker.updateLocaiton(new int[]{col*2+row%2,row});
+				pieces.get("posMove").clear();
+				
 			}
 		}
 		for (Sprite possibleMove : possibleMoves) {
 			possibleMove.draw(batch);
 		}
+		
+		
 		
 		batch.end();
 	}
@@ -141,13 +175,17 @@ public class Checkers extends Game implements Screen {
 
 	}
 	
+	private String getOtherColor(String color) {
+		if (colors[1].equals(color)) return colors[1];
+		else return colors[0];
+	}
 	
 	private void setBoard() {
 		for (int row = 0; row < 3; row++) {
 			for (int col = 0; col <= 7; col+=2) {
 				if (row == 1 && col == 0) col++;
-				pieces.get(colors[0]).add(new CheckerPiece(false, new int[]{col,row}));
-				pieces.get(colors[1]).add(new CheckerPiece(true, new int[]{7-col,7-row}));
+				pieces.get(colors[0]).add(new CheckerPiece(1, new int[]{col,row}));
+				pieces.get(colors[1]).add(new CheckerPiece(2, new int[]{7-col,7-row}));
 			}
 		}
 	}
@@ -156,26 +194,41 @@ public class Checkers extends Game implements Screen {
 		ArrayList<int[]> possibleMoves = new ArrayList<int[]>();
 		if (gameBoardArr[row][col] == 0) throw new NoSuchElementException();
 		try { //JUMP UP LEFT
-			if(gameBoardArr[row+2][col-1] == 0 && gameBoardArr[row+1][col-1+row%2] != gameBoardArr[row][col] && gameBoardArr[row+1][col-1+row%2] != 0) possibleMoves.add(new int[] {row+2,col-1}); //If: up2&left is empty, up&left is not empty, up&left is not same
+			if(gameBoardArr[row+2][col-1] %3 == 0 && gameBoardArr[row+1][col-1+row%2] != gameBoardArr[row][col] && gameBoardArr[row+1][col-1+row%2] %3 != 0) possibleMoves.add(new int[] {row+2,col-1}); //If: up2&left is empty, up&left is not empty, up&left is not same
 		} catch(IndexOutOfBoundsException e) {};
 		try { //JUMP UP RIGHT
-			if(gameBoardArr[row+2][col+1] == 0 && gameBoardArr[row+1][col+row%2] != gameBoardArr[row][col] &&  gameBoardArr[row+1][col+row%2] != 0) possibleMoves.add(new int[] {row+2,col+1});//If: up2&right is empty, up&right is not empty, up&right is not same
+			if(gameBoardArr[row+2][col+1] %3 == 0 && gameBoardArr[row+1][col+row%2] != gameBoardArr[row][col] &&  gameBoardArr[row+1][col+row%2] %3 != 0) possibleMoves.add(new int[] {row+2,col+1});//If: up2&right is empty, up&right is not empty, up&right is not same
 		} catch(IndexOutOfBoundsException e) {};
 		try { //JUMP UP LEFT
-			if(gameBoardArr[row+1][col-1+row%2] == 0) possibleMoves.add(new int[] {row+1,col-1+row%2}); //If: up&left is empty
+			if(gameBoardArr[row+1][col-1+row%2] %3 == 0) possibleMoves.add(new int[] {row+1,col-1+row%2}); //If: up&left is empty
 		} catch(IndexOutOfBoundsException e) {};
 		try { //MOVE UP RIGHT
-			if(gameBoardArr[row+1][col+row%2] == 0) possibleMoves.add(new int[] {row+1,col+row%2});//If: up&right is empty
+			if(gameBoardArr[row+1][col+row%2] %3 == 0) possibleMoves.add(new int[] {row+1,col+row%2});//If: up&right is empty
+		} catch(IndexOutOfBoundsException e) {};
+		try { //JUMP DOWN LEFT
+			if(gameBoardArr[row-2][col-1] %3 == 0 && gameBoardArr[row-1][col-1+row%2] != gameBoardArr[row][col] && gameBoardArr[row-1][col-1+row%2] %3 != 0) possibleMoves.add(new int[] {row-2,col-1}); //If: down2&left is empty, up&left is not empty, up&left is not same
+		} catch(IndexOutOfBoundsException e) {};
+		try { //JUMP DOWN RIGHT
+			if(gameBoardArr[row-2][col+1] %3 == 0 && gameBoardArr[row-1][col+row%2] != gameBoardArr[row][col] &&  gameBoardArr[row-1][col+row%2] %3 != 0) possibleMoves.add(new int[] {row-2,col+1});//If: down2&right is empty, up&right is not empty, up&right is not same
+		} catch(IndexOutOfBoundsException e) {};
+		try { //JUMP DOWN LEFT
+			if(gameBoardArr[row-1][col-1+row%2] %3 == 0) possibleMoves.add(new int[] {row-1,col-1+row%2}); //If:downp&left is empty
+		} catch(IndexOutOfBoundsException e) {};
+		try { //MOVE DOWN RIGHT
+			if(gameBoardArr[row-1][col+row%2] %3 == 0) possibleMoves.add(new int[] {row-1,col+row%2});//If: down&right is empty
 		} catch(IndexOutOfBoundsException e) {};
 		
 		return possibleMoves;
 	}
 	
-	private CheckerPiece getPieceByPosition(int row, int col) {
+	private String getPieceByPosition(int row, int col) {
 		for (String color : colors) {
-			for (CheckerPiece checker : pieces.get(color)) {
-				if(checker.getCol() == col && checker.getRow() == row) return checker;
+			for (int i = 0; i < pieces.get(color).size(); i++) {
+				if(pieces.get(color).get(i).getCol() == col && pieces.get(color).get(i).getRow() == row) return color + " " + i;
 			}
+//			for (CheckerPiece checker : pieces.get(color)) {
+//				if(checker.getCol() == col && checker.getRow() == row) return checker;
+//			}
 		}
 		return null;
 	}
